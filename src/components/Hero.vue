eaaa13709c66c3ed7ca5a67810e122e4b08380e1
albarstring <rizqizulmiaztaalbar1@gmail.com>
2025-07-13 16:36:51 +0700

    fix scroll indicator

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -73,8 +73,8 @@
 
     <!-- Scroll Indicator -->
     <div class="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-      <div class="w-6 h-10 border-2 border-white/30 rounded-full flex justify-center">
-        <div class="w-1 h-3 bg-white/60 rounded-full mt-2 animate-pulse"></div>
+      <div class="w-6 h-10 border-2 border-gray-800/30 dark:border-white/30 rounded-full flex justify-center">
+        <div class="w-1 h-3 bg-gray-800/60 dark:bg-white/60 rounded-full mt-2 animate-pulse"></div>
       </div>
     </div>
   </section>
